3f1904e3a0624195826097edd4cbd24629e0ac58
JorgePaz02 <jorge.paz02@utrgv.edu>
2023-11-27 21:21:35 -0600

    QuitgroupReset

--- lib/UserInfo/QuitGroupReset.dart
+++ lib/UserInfo/QuitGroupReset.dart
@@ -3,20 +3,16 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 Future<void> userReset(String groupCode) async {
- 
   final db = FirebaseFirestore.instance;
   db.collection("Groups").doc(groupCode).get().then((value) async {
     List members = value.get("members");
     for (var i in members) {
       final docRef = db.collection("Users").doc(i);
       docRef.update({
-        
         "group": "",
         "group leader": false,
       });
-
     }
     db.collection("Groups").doc(groupCode).delete();
-    
   });
 }
