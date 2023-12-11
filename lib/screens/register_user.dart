de1ef2268a05dbecb165f8091b94932469d51416
gxbz <g.uribe95@hotmail.com>
2023-12-11 08:06:25 -0600

    .

--- lib/screens/register_user.dart
+++ lib/screens/register_user.dart
@@ -1,7 +1,7 @@
-import 'package:app/screens/login_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:app/UserInfo/usermodel.dart';
+import 'package:app/screens/login_screen.dart';
 
 class Registration extends StatefulWidget {
   const Registration({Key? key}) : super(key: key);
@@ -11,7 +11,6 @@ class Registration extends StatefulWidget {
 }
 
 class _RegistrationState extends State<Registration> {
-
   String emailError = "";
   String passwordError = "";
 
@@ -21,6 +20,8 @@ class _RegistrationState extends State<Registration> {
     final passwordController = TextEditingController();
 
     return Scaffold(
+      extendBodyBehindAppBar:
+          true, // Add this line to extend body behind the app bar
       appBar: AppBar(
         backgroundColor: Colors.transparent,
         elevation: 0,
@@ -31,137 +32,156 @@ class _RegistrationState extends State<Registration> {
           },
         ),
       ),
-      body: SingleChildScrollView(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              "Sign Up",
-              style: TextStyle(
-                fontSize: 24.0,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-             const SizedBox(
-              height: 30.0
-            ),
-             Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: TextField(
-                controller: emailController,
-                decoration: InputDecoration(
-                  labelText: "Email",
-                  border: const OutlineInputBorder(),
-                  errorText: emailError.isEmpty ? null : emailError,
+      body: Stack(
+        fit: StackFit.expand,
+        children: [
+          Image.asset(
+            'lib/assets/regular.png', // Update the path to your image
+            fit: BoxFit.cover,
+          ),
+          SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                const Text(
+                  "Sign Up",
+                  style: TextStyle(
+                    fontSize: 24.0,
+                    fontWeight: FontWeight.bold,
+                  ),
                 ),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: TextField(
-                controller: passwordController,
-                decoration: InputDecoration(
-                  labelText: "Password",
-                  border: const OutlineInputBorder(),
-                  errorText: passwordError.isEmpty ? null : passwordError,
+                const SizedBox(
+                  height: 30.0,
                 ),
-                obscureText: true,
-              ),
-            ),
-            ElevatedButton(
-              onPressed: () async {
-              try {
-                await FirebaseAuth.instance.createUserWithEmailAndPassword(
-                  email: emailController.text,
-                  password: passwordController.text,
-                );
-                User? updateUser = FirebaseAuth.instance.currentUser;
-                updateUser!.updateDisplayName(emailController.text);
-                userSetup(emailController.text);
-                Navigator.push(context, MaterialPageRoute(builder: (context) => LoginScreen()));
-              } on FirebaseAuthException catch (e) {
-                switch (e.code) {
-                  case 'email-already-in-use':
-                    setState(() {
-                      emailError = 'The email address provided is already in use for an account.';
-                      passwordError = '';
-                    });
-                    break;
-                  case 'invalid-email':
-                    setState(() {
-                      emailError = 'The email address provided is invalid.';
-                      passwordError = '';
-                    });
-                    break;
-                  case 'weak-password':
-                    setState(() {
-                      passwordError = 'The password provided is not strong enough for use.';
-                      emailError = '';
-                    });
-                    break;
-                  case 'channel-error':
-                    setState(() {
-                      emailError = emailController.text.isEmpty ? 'The email address provided is empty.' : '';
-                      passwordError = passwordController.text.isEmpty ? 'The password provided is empty.' : '';
-                    });
-                    break;     
-                }
-              } catch (e) {
-                setState(() {
-                  passwordError = 'There has been an error. Please try again.';
-                  emailError = 'There has been an error. Please try again.';
-                });
-              }
-        
-                // Add your Sign up button functionality here
-
-              },
-              style: ElevatedButton.styleFrom(
-                foregroundColor: Colors.white, backgroundColor: Colors.black, // Text color
-                padding: const EdgeInsets.symmetric(horizontal: 40, vertical: 16),
-                textStyle: const TextStyle(
-                  fontSize: 18,
+                Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: TextField(
+                    controller: emailController,
+                    decoration: InputDecoration(
+                      labelText: "Email",
+                      border: const OutlineInputBorder(),
+                      errorText: emailError.isEmpty ? null : emailError,
+                    ),
+                  ),
                 ),
-              ),
-              child: const Text("Sign Up"),
+                Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: TextField(
+                    controller: passwordController,
+                    decoration: InputDecoration(
+                      labelText: "Password",
+                      border: const OutlineInputBorder(),
+                      errorText: passwordError.isEmpty ? null : passwordError,
+                    ),
+                    obscureText: true,
+                  ),
+                ),
+                ElevatedButton(
+                  onPressed: () async {
+                    try {
+                      await FirebaseAuth.instance
+                          .createUserWithEmailAndPassword(
+                        email: emailController.text,
+                        password: passwordController.text,
+                      );
+                      User? updateUser = FirebaseAuth.instance.currentUser;
+                      updateUser!.updateDisplayName(emailController.text);
+                      userSetup(emailController.text);
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => LoginScreen()));
+                    } on FirebaseAuthException catch (e) {
+                      switch (e.code) {
+                        case 'email-already-in-use':
+                          setState(() {
+                            emailError =
+                                'The email address provided is already in use for an account.';
+                            passwordError = '';
+                          });
+                          break;
+                        case 'invalid-email':
+                          setState(() {
+                            emailError =
+                                'The email address provided is invalid.';
+                            passwordError = '';
+                          });
+                          break;
+                        case 'weak-password':
+                          setState(() {
+                            passwordError =
+                                'The password provided is not strong enough for use.';
+                            emailError = '';
+                          });
+                          break;
+                        case 'channel-error':
+                          setState(() {
+                            emailError = emailController.text.isEmpty
+                                ? 'The email address provided is empty.'
+                                : '';
+                            passwordError = passwordController.text.isEmpty
+                                ? 'The password provided is empty.'
+                                : '';
+                          });
+                          break;
+                      }
+                    } catch (e) {
+                      setState(() {
+                        passwordError =
+                            'There has been an error. Please try again.';
+                        emailError =
+                            'There has been an error. Please try again.';
+                      });
+                    }
+                  },
+                  style: ElevatedButton.styleFrom(
+                    foregroundColor: Colors.white,
+                    backgroundColor: Colors.black, // Text color
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 40, vertical: 16),
+                    textStyle: const TextStyle(
+                      fontSize: 18,
+                    ),
+                  ),
+                  child: const Text("Sign Up"),
+                ),
+              ],
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
 }
 
-
-
 Future<User?> registerUsingEmailPassword({
-    required String name,
-    required String email,
-    required String password,
-  }) async {
-    FirebaseAuth auth = FirebaseAuth.instance;
-    User? user;
+  required String name,
+  required String email,
+  required String password,
+}) async {
+  FirebaseAuth auth = FirebaseAuth.instance;
+  User? user;
 
-    try {
-      UserCredential userCredential =
-          await FirebaseAuth.instance.createUserWithEmailAndPassword(
-        email: email,
-        password: password,
-      );
+  try {
+    UserCredential userCredential =
+        await FirebaseAuth.instance.createUserWithEmailAndPassword(
+      email: email,
+      password: password,
+    );
 
-      user = userCredential.user;
-      await user!.updateDisplayName(name);
-      await user.reload();
-      user = auth.currentUser;
-    } on FirebaseAuthException catch (e) {
-      if (e.code == 'weak-password') {
-        print('The password provided is too weak.');
-      } else if (e.code == 'email-already-in-use') {
-        print('The account already exists for that email.');
-      }
-    } catch (e) {
-      print(e);
+    user = userCredential.user;
+    await user!.updateDisplayName(name);
+    await user.reload();
+    user = auth.currentUser;
+  } on FirebaseAuthException catch (e) {
+    if (e.code == 'weak-password') {
+      print('The password provided is too weak.');
+    } else if (e.code == 'email-already-in-use') {
+      print('The account already exists for that email.');
     }
-
-    return user;
+  } catch (e) {
+    print(e);
   }
+
+  return user;
+}
